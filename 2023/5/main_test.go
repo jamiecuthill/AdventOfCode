e4c287419c6266408fb7e1b2a4dbc325e8e8d668
Jamie Cuthill <jamie.cuthill@justeattakeaway.com>
2023-12-05 22:08:16 +0000

    Day 5 - SLOW part 2

---
+++ 2023/5/main_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestPart1Example(t *testing.T) {
+	assert.Equal(t, 35, Part1(bufio.NewScanner(strings.NewReader(`seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`))))
+}
+
+func TestPart2Example(t *testing.T) {
+	assert.Equal(t, 46, Part2(bufio.NewScanner(strings.NewReader(`seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`))))
+}
+
+func TestPart1(t *testing.T) {
+	f, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	input := bufio.NewScanner(f)
+
+	assert.Equal(t, 403695602, Part1(input))
+}
+
+func TestPart2(t *testing.T) {
+	f, err := os.Open("input")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	input := bufio.NewScanner(f)
+
+	assert.Equal(t, 219529182, Part2(input))
+}
